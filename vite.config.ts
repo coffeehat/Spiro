4174402817061c71a51bb76861e2e4e3d18f64e6
Hanxiang.Li <hanx.leee@gmail.com>
2023-01-27 16:12:47 +0800

    Add rollup-plugin-visualizer to analyze js size

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,17 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), visualizer()],
   build: {
     sourcemap: true,
     lib: {
       entry: resolve(__dirname, "src/spiro.ts"),
       name: "spiro"
-    },
-    // rollupOptions: {
-    //   external: ['vue', 'pinia'],
-    //   output: {
-    //     globals: {
-    //       vue: 'Vue'
-    //     },
-    //     paths: {
-    //       vue: 'https://unpkg.com/vue@3/dist/vue.esm-browser.js',
-    //       pinia: 'https://cdnjs.cloudflare.com/ajax/libs/pinia/2.0.29/pinia.esm-browser.min.js'
-    //     }
-    //   }
-    // }
+    }
   },
   define: {
     'process.env.NODE_ENV': '"production"'
